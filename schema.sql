5380d2ca474fc0b580157bef6a40613a8de0fcaf
Iain R. Learmonth <irl@sdf.org>
2013-12-24 15:10:11 +0000

    Added an sqlite3 database schema

---
+++ schema.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE IF NOT EXISTS `fitbit_steps` (
+    start STRING,
+    end STRING,
+    steps INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS `fitbit_sleep` (
+    start STRING,
+    end STRING,
+    state INTEGER
+);
+
+

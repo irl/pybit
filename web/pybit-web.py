7b6a1799e2382c79d8dc649e8f75d2c30b47eb35
Iain R. Learmonth <irl@sdf.org>
2013-12-25 19:41:23 +0000

    Allowed the selection of different dates in the web interface

--- web/pybit-web.py
+++ web/pybit-web.py
@@ -6,7 +6,7 @@
 # For redistribution and use terms, see the LICENSE file.
 #
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import sqlite3
 import datetime
 
@@ -16,9 +16,21 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route('/')
 def home():
+    today = datetime.date.today()
+    year = today.year
+    month = today.month
+    day = today.day
+    return redirect('/%d/%d/%d' % (year, month, day))
+
+@app.route('/<year>/<month>/<day>')
+def summary(year, month, day):
+    print((year, month, day))
+    date = datetime.date(int(year), int(month), int(day))
+    print(date)
+    print(datetime.date.today())
     db = sqlite3.connect(DB_PATH)
     cur = db.cursor()
-    cur.execute("SELECT * FROM `fitbit_steps` WHERE datetime(`start`) >= datetime(?)", (datetime.datetime.combine(datetime.date.today(), datetime.time(0,0)).strftime('%Y-%m-%dT%H:%M'),))
+    cur.execute("SELECT * FROM `fitbit_steps` WHERE datetime(`start`) >= datetime(?) AND datetime(`start`) < datetime(?)", (datetime.datetime.combine(date, datetime.time(0,0)).strftime('%Y-%m-%dT%H:%M'), ((datetime.datetime.combine(date, datetime.time(0,0)) + datetime.timedelta(days = 1)).strftime('%Y-%m-%dT%H:%M'))))
     result = cur.fetchall()
     stepsPlot = []
     for row in result:
@@ -26,14 +38,14 @@ def home():
         pos = (ts.hour * 60) + ts.minute
         stepsPlot.append((pos, row[2]))
     print(stepsPlot)
-    cur.execute("SELECT * FROM `fitbit_sleep` WHERE datetime(`start`) >= datetime(?)", (datetime.datetime.combine(datetime.date.today(), datetime.time(0,0)).strftime('%Y-%m-%dT%H:%M'),))
+    cur.execute("SELECT * FROM `fitbit_sleep` WHERE datetime(`start`) >= datetime(?) AND datetime(`start`) < datetime(?)", (datetime.datetime.combine(date, datetime.time(0,0)).strftime('%Y-%m-%dT%H:%M'), ((datetime.datetime.combine(date, datetime.time(0,0)) + datetime.timedelta(days = 1)).strftime('%Y-%m-%dT%H:%M'))))
     result = cur.fetchall()
     sleepPlot = []
     for row in result:
         ts = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
         pos = (ts.hour * 60) + ts.minute
         sleepPlot.append((pos, row[2]))
-    return render_template('steps.html', sleepPlot = sleepPlot, stepsPlot = stepsPlot, date = datetime.date.today())
+    return render_template('steps.html', sleepPlot = sleepPlot, stepsPlot = stepsPlot, date = date)
 
 if __name__ == "__main__":
     app.debug = True
